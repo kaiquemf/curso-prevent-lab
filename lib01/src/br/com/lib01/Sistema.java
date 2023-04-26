6514e181b1dd13e16227fe8456875595e00b82a2
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2023-04-26 07:24:10 -0300

    Modify Plan Validation to static

--- lib01/src/br/com/lib01/Sistema.java
+++ lib01/src/br/com/lib01/Sistema.java
@@ -6,5 +6,12 @@ public class Sistema {
 		
 	Beneficiario benef1 = new Beneficiario("Kaique Ferreira", "49311007838", 
 				18, true);
+	
+	Beneficiario benef2 = new Beneficiario("Kaique Ferreira", "49311007838", 
+			18, false);
+	
+	ValidacoesAcesso.ValidacaoPlanoAtivo(benef1);
+	System.out.println();
+	ValidacoesAcesso.ValidacaoPlanoAtivo(benef2);
 	}
 }
--- lib01/src/br/com/lib01/ValidacoesAcesso.java
+++ lib01/src/br/com/lib01/ValidacoesAcesso.java
@@ -2,11 +2,11 @@ package br.com.lib01;
 
 public class ValidacoesAcesso {
 	
-	public void ValidacaoPlanoAtivo(Beneficiario beneficiario) {
+	public static void ValidacaoPlanoAtivo(Beneficiario beneficiario) {
 		if(beneficiario.beneficiarioAtivo == true) {
 			System.out.println("O plano está ativo!");
 		} else {
-			System.out.println("O plano está inativo" +
+			System.out.println("O plano está inativo," +
 					" Por favor efetue o pagamento!");
 		}
 	}

f904fbd5a5c63b15ca1b03881e3c68e784c15e3d
Kaique Martins <kaiquemartinsdasilvaferreira@gmail.com>
2023-04-26 06:50:15 -0300

    create benef props

---
+++ lib01/src/br/com/lib01/Beneficiario.java
@@ -0,0 +1,8 @@
+package br.com.lib01;
+
+public class Beneficiario {
+	private    String beneficiarioNome;
+	private    String beneficiarioCpf;
+	private 	  int beneficiarioIdade;
+	protected boolean beneficiarioAtivo;
+}
